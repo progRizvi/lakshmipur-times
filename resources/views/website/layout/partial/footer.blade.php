5b8267402da101c94155e71e5800d4a98350470b
progRizvi <prog.rizvi@gmail.com>
2024-10-15 23:57:53 +0600

    footer menu dynamic

--- resources/views/website/layout/partial/footer.blade.php
+++ resources/views/website/layout/partial/footer.blade.php
@@ -5,12 +5,12 @@
             <div class="flex max-[768px]:flex-col items-center justify-between gap-5 mb-4">
                 <!-- Logo -->
                 <div class="flex items-center space-x-2">
-                    <img class="h-36" src="/src/images/LakshmipurTimesLogo.png" alt="" />
+                    <img class="h-36" src="{{ asset($setting->logo) }}" alt="{{ $setting->app_name }}" />
                 </div>
 
                 <!-- Editor's Name -->
                 <div>
-                    <span class="text-gray-700 font-bold"> সম্পাদকঃ আনিস কবির </span>
+                    <span class="text-gray-700 font-bold"> সম্পাদকঃ {{ $setting->editor }} </span>
                 </div>
             </div>
 
@@ -18,63 +18,53 @@
             <div class="flex flex-col justify-between items-center pt-4">
                 <!-- social media icons start -->
                 <div class="flex items-center gap-5 mb-3">
-                    <div>
-                        <a class="social_icon_style" href="https://www.facebook.com/share/3ZvmEXddzX6hbsRT/">
-                            <i class="fa-brands fa-facebook-f"></i>
-                        </a>
-                    </div>
-                    <div>
-                        <a class="social_icon_style" href="#">
-                            <i class="fa-brands fa-instagram"></i>
-                        </a>
-                    </div>
-                    <div>
-                        <a class="social_icon_style" href="https://wa.me/01998304701">
-                            <i class="fa-brands fa-whatsapp"></i>
-                        </a>
-                    </div>
-                    <div>
-                        <a class="social_icon_style" href="mailto:lakshmipurtimes24@gmail.com">
-                            <i class="fa-regular fa-envelope"></i>
-                        </a>
-                    </div>
-
-                    <div>
-                        <a class="social_icon_style" href="https://www.youtube.com/@lakshmipurtimes-24">
-                            <i class="fa-brands fa-youtube"></i>
-                        </a>
-                    </div>
+                    @php
+                        $socials = Modules\SocialLink\app\Models\SocialLink::where('status', 1)->get();
+                    @endphp
+                    @foreach ($socials as $social)
+                        <div>
+                            <a class="social_icon_style" href="{{ $social->link }}">
+                                <i class="{{ $social->icon }}"></i>
+                            </a>
+                        </div>
+                    @endforeach
                 </div>
                 <!-- social media icons end -->
+
+                @php
+                    if (cache()->has('footer_menu')) {
+                        $footer = cache('footer_menu');
+                    } else {
+                        $footer = Cache::rememberForever('footer_menu', function () {
+                            return \Modules\CustomMenu\app\Models\Menu::with('items.translation')
+                                ->where('slug', 'secondary-menu')
+                                ->first();
+                        });
+                    }
+                @endphp
                 <!-- Links start -->
                 <div
                     class="flex max-[768px]:flex-wrap max-[768px]:justify-center max-[768px]:gap-4 space-x-4 text-sm text-gray-600 mb-3">
-                    <a href="./privacy_policy.html" class="hover:underline hover:primary_text_color">
-                        গোপনীয়তার নীতি
-                    </a>
-                    <a href="./contact.html" class="hover:underline hover:primary_text_color">
-                        যোগাযোগ
-                    </a>
-                    <a href="./our_team.html" class="hover:underline hover:primary_text_color">
-                        আমরা
-                    </a>
-                    <a href="./advertise.html" class="hover:underline hover:primary_text_color">
-                        বিজ্ঞাপণ
-                    </a>
+                    @foreach ($footer->items as $item)
+                        <a href="{{ $item->link }}" class="hover:underline hover:primary_text_color">
+                            {{ $item->label }}
+                        </a>
+                    @endforeach
                 </div>
+
                 <!-- Links end -->
 
                 <!-- Contact Info start -->
                 <div class="text-sm text-gray-600">
                     <p class="text-center mb-3">
-                        বাগবাড়ি, লক্ষ্মীপুর সদর, লক্ষ্মীপুর - ৩৭০০।
+                        {{ $setting->address }}
                     </p>
 
                     <div
                         class="flex max-[768px]:flex-wrap max-[768px]:justify-center max-[768px]:gap-4 items-center justify-center gap-5">
                         <p class="flex items-center">
                             <span class="mr-2">
-                                <i class="fa-solid fa-mobile-screen-button"></i>
+                                <i class="fas fa-mobile-alt"></i>
                             </span>
                             <a href="tel:+8801998304701" class="hover:underline hover:primary_text_color">
                                 +৮৮০ ১৯৯৮ ৩০৪৭০১
@@ -82,7 +72,7 @@
                         </p>
                         <p class="flex items-center">
                             <span class="mr-2">
-                                <i class="fa-brands fa-whatsapp"></i>
+                                <i class="fab fa-whatsapp"></i>
                             </span>
                             <a href="https://wa.me/01998304701" class="hover:underline hover:primary_text_color">
                                 +৮৮০ ১৯৯৮ ৩০৪৭০১
@@ -90,11 +80,10 @@
                         </p>
                         <p class="flex items-center">
                             <span class="mr-2">
-                                <i class="fa-regular fa-envelope"></i>
+                                <i class="fas fa-envelope"></i>
                             </span>
-                            <a href="mailto:lakshmipurtimes24@gmail.com"
-                                class="hover:underline hover:primary_text_color">
-                                lakshmipurtimes24@gmail.com
+                            <a href="mailto:{{ $setting->email }}" class="hover:underline hover:primary_text_color">
+                                {{ $setting->email }}
                             </a>
                         </p>
                     </div>
@@ -103,7 +92,7 @@
 
                 <!-- copyright start -->
                 <div class="mt-5 mb-1 py-2 px4 text-center border-t border-[#ff5f15] text-slate-900 w-full">
-                    <p>&copy; 2024, Lakshmipur Times. All Rights Reserved.</p>
+                    <p>{{ $setting->copyright_text }}</p>
                 </div>
                 <!-- copyright end -->
             </div>
